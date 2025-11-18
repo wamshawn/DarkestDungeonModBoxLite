0132440459beb48c52a5ae7fedd2f190ee0f5d6d
wangminxiang <ryougi.nevermore@hotmail.com>
2025-11-18 19:57:10 +0800

    mod

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import './style.css'
-import App from './App'
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
+import {App, ConfigProvider, theme} from 'antd';
+import {RouterProvider} from "react-router-dom";
+import router from "./router/router";
+
 
 const container = document.getElementById('root')
 
 const root = createRoot(container!)
 
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <DevSupport ComponentPreviews={ComponentPreviews}
                     useInitialHook={useInitial}
         >
-            <App/>
+            <ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
+                <App>
+                    <RouterProvider router={router} />
+                </App>
+            </ConfigProvider>
         </DevSupport>
-    </React.StrictMode>
+    // </React.StrictMode>
 )
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"embed"
+
+	"DarkestDungeonModBoxLite/backend"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -13,7 +16,7 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	app := backend.New()
 
 	// Create application with options
 	err := wails.Run(&options.App{
@@ -24,9 +27,18 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
+		OnStartup:        app.Startup,
+		OnBeforeClose: func(ctx context.Context) (prevent bool) {
+			app.Shutdown(ctx)
+			return false
+		},
+		OnShutdown: app.Shutdown,
+		Bind:       app.Binds(),
+		SingleInstanceLock: &options.SingleInstanceLock{
+			UniqueId: app.Id(),
+			OnSecondInstanceLaunch: func(secondInstanceData options.SecondInstanceData) {
+				app.Show()
+			},
 		},
 	})
 
